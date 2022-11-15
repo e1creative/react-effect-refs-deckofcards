ef20d256b80cfc39e22c214e3bf0e9ec0c7ae2f1
Jason Takahashi <jasontakahashi@Jasons-MacBook-Pro.local>
2022-11-15 13:43:14 -0800

    Update with useRef.

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 import Card from './Card'
@@ -6,54 +6,54 @@ import Card from './Card'
 const Deck = () => {
     const INITIAL_STATE = []
     const [cards, setCards] = useState(INITIAL_STATE);
-    const [deckID, setDeckID] = useState("");
+    const [deckID, setDeckID] = useState(null)
     const [remainingCards, setRemainingCards] = useState(52)
-    const [intervalID, setIntervalID] = useState();
+    const [startDeal, setStartDeal] = useState(false);
+    
+    // store our persistent info that won't change on renders
+    const intervalID = useRef();
 
     const handleClick = (e) => {
-        if (intervalID) {
-            clearInterval(intervalID)
-        } else {
-            // change our button text
-            e.target.innerText = "Stop Drawing"
-
+        if (!startDeal) {
             // set the intervalID state.  make sure to the return the set interval function
-            setIntervalID(() => {
-                return setInterval(async () => {
-                    const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`);
-                    const { remaining } = res.data
-                    const { code, image, value, suit } = res.data.cards[0]
-                    const rotate = Math.floor(Math.random() * 15 - 10);
-                    setCards(data => [...data, { code, image, value, suit, rotate: rotate+"deg" }])
-                    setRemainingCards(remaining)    
-                }, 1000)
-            });
+            intervalID.current = setInterval(async () => {
+                const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`);
+                const { remaining } = res.data
+                const { code, image, value, suit } = res.data.cards[0]
+                const rotate = Math.floor(Math.random() * 21 - 10);
+                setCards(data => [...data, { code, image, value, suit, rotate: rotate+"deg" }])
+                setRemainingCards(remaining)    
+            }, 1000)
+        } else {
+            clearInterval(intervalID.current)
         }
-        console.log(intervalID);
+
+        // set startDeal state to true or false depending on what the current val is
+        setStartDeal(!startDeal)
     }
 
-    // run shuffle deck only once, on load!  since we only run once, we need to store the deck id in state!
-    // we can then use the deckID (state) to make our draw card axios call
+    // run shuffle deck only once, on load!
     useEffect(() => {
         async function shuffleDeck() {
             const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
-            const deck_id = res.data.deck_id
-            setDeckID(deck_id)
+            setDeckID(res.data.deck_id)
         };
         shuffleDeck();
     }, [])
 
 
-
     let msg;
-    if (deckID && remainingCards !== 0) { 
-        msg = <button onClick={handleClick}>Start Drawing</button>
-    } else if (deckID && remainingCards === 0) {
-        clearInterval(intervalID)
-        msg = <p>Error: no cards remaining!</p>
+
+    if (deckID) {
+        if (remainingCards === 0) { 
+            clearInterval(intervalID.current)
+            msg = <p>Error: no cards remaining!</p>
+        } else { 
+            msg = <button onClick={handleClick}>Start/Stop Drawing</button> 
+        } 
     } else {
         msg = <p>Loading...</p>
-    };
+    }
 
     return (
         <div className="Deck">
